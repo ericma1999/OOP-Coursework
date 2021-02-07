af644b84635a15e6cf2326a462f63a33fd6b5136
Passawis <official.passawis@gmail.com>
2021-02-07 16:54:08 +0700

    removed some unused comments

--- Coursework/src/uk/ac/ucl/cs/passawis/Main.java
+++ Coursework/src/uk/ac/ucl/cs/passawis/Main.java
@@ -4,9 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Menu menu = new Menu();
-//        main.Dictionary dictionary = new main.Dictionary("words");
-//        main.SpellChecker spellChecker = new main.SpellChecker(dictionary, 4);
-
+        
         Controller applicationController = new Controller(menu);
 
         applicationController.start();
